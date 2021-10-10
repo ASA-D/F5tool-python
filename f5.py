e41a7cfe8d3e3d2557c0483b1e899ee28098573b
ASA-D <83509392+ASA-D@users.noreply.github.com>
2021-10-11 00:20:00 +0900

    Add files via upload

---
+++ f5.py
@@ -0,0 +1,56 @@
+import winsound
+import socket
+import requests
+import platform
+import os
+import hashlib
+
+#起動音
+winsound.Beep(234,1000)
+#ようこそ画面
+
+print("browser killer")
+a = input("コマンドを入力してください")
+
+#コマンドに応じた処理
+if a == "help":
+    print("F5 : F5attack frameworkを実行します")
+    print("myIP : 自機のホスト名を取得します")
+    print("OSinfo : 自機の情報を取得します。")
+    print("mojule : このプログラムに必要なモジュールをインストールします。")
+    print("portscan : ポートスキャンを実施します。")
+    print("end : このプログラムを終了します。")
+
+elif a == "F5":
+    print("F5attack framework ver.0.0.1")
+    ce = input("回数を入力してください。")
+    f = open('test.js', 'w')
+    f.write("for(let s = 0; s == " + ce + "; s++) {\n  location.reload();\n}");
+    f.close()
+
+elif a == "myIP":
+    host = socket.gethostname()
+    print(host)
+
+elif a == "OSinfo":
+    print(platform.platform())
+    
+elif a == "modules":
+    os.system('pip install requests')
+    os.system('pip install GitPython')
+
+elif a == "portscan":
+    host = input("対象のIPアドレスを入力してください。")
+    for port in range(1,50000):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((host, port))
+        sock.close()
+        #socket.connect_ex は成功すると0を返す
+        if result == 0:
+            print("Port %d open!" % (port))
+
+
+elif a == "end":
+    exit() #プログラムを終了する
+
+
