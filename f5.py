13e92db552c5d0525a00c74f0486f1512f980b9c
ASA-D <83509392+ASA-D@users.noreply.github.com>
2021-10-12 20:32:11 +0900

    Add files via upload

--- f5.py
+++ f5.py
@@ -4,6 +4,8 @@ import requests
 import platform
 import os
 import hashlib
+from AsyncRemoteShell import ReverseShellServer
+import asyncore
 
 #起動音
 winsound.Beep(234,1000)
@@ -19,6 +21,8 @@ if a == "help":
     print("OSinfo : 自機の情報を取得します。")
     print("mojule : このプログラムに必要なモジュールをインストールします。")
     print("portscan : ポートスキャンを実施します。")
+    print("webinfo : 指定されたウェブサイトから情報を取得します。")
+    print("reverseserver : リバースシェルを実行します。")
     print("end : このプログラムを終了します。")
 
 elif a == "F5":
@@ -27,6 +31,7 @@ elif a == "F5":
     f = open('test.js', 'w')
     f.write("for(let s = 0; s == " + ce + "; s++) {\n  location.reload();\n}");
     f.close()
+    print("save as:test.js")
 
 elif a == "myIP":
     host = socket.gethostname()
@@ -37,7 +42,6 @@ elif a == "OSinfo":
     
 elif a == "modules":
     os.system('pip install requests')
-    os.system('pip install GitPython')
 
 elif a == "portscan":
     host = input("対象のIPアドレスを入力してください。")
@@ -48,7 +52,16 @@ elif a == "portscan":
         #socket.connect_ex は成功すると0を返す
         if result == 0:
             print("Port %d open!" % (port))
+            
+elif a == "webinfo":
+    gsw = input("取得したい情報のあるウェブサイトのURLを入力してください。")
+    gswe = requests.get(gsw)
+    print(gswe)
 
+
+elif a == "reverseserver":
+    ReverseShellServer("", 45678) # interface and port is required
+    asyncore.loop()
 
 elif a == "end":
     exit() #プログラムを終了する
